e92795eafacbe9e102220900fc77ba8aaa9a9938
semaaj <mamesjiles@gmail.com>
2020-10-30 10:27:10 +0000

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Homepage from './components/Homepage';
+import ReactGA from 'react-ga';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-0BNQT4Q2CV');
+    ReactGA.pageview('/');
+  })
+
   return (
     <div className="App">
       <Homepage />
